b3f13fa7e53ee51d18fda7a16f7c865f20cb97ff
Ivan Muñoz <ivandariomunioz@gmail.com>
2024-01-02 14:47:18 -0300

    API models

--- evaluacion/API/views.py
+++ evaluacion/API/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import pandas as pd
 from sqlalchemy import create_engine
+from settings import DATABASES
 
 
 # Ruta del archivo
@@ -10,4 +11,8 @@ archivo_csv = '../data/data.csv'
 def script_ETL():
     # Cargar el archivo CSV en un DataFrame
     df = pd.read_csv(archivo_csv, header=0)
+    #  Nombrar columnas
     df.columns = ['fecha','tipo_de_cambio.']
+
+    #conexion a DB usando sqlalchemy
+    engine = create_engine(DATABASES)
